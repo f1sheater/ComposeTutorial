f7471b8502ab69b2937f74fae595ca426871347f
f1sheater <hsulin22@student.oulu.fi>
2025-02-11 18:17:26 +0200

    HW-3

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -3,46 +3,30 @@ package com.example.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.border
-import androidx.compose.material3.MaterialTheme
-import android.content.res.Configuration
 import com.example.composetutorial.ui.theme.ComposeTutorialTheme
-import androidx.compose.ui.res.painterResource
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.clickable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.animateContentSize
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import kotlinx.serialization.Serializable
+import java.io.File
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            //val context = LocalContext.current
+            //val resolver = context.contentResolver
+            //val picture = File(context.filesDir, "picture")
+            //val nameFile = File(context.filesDir, "name")
             ComposeTutorialTheme {
                 Column {
                     Spacer(modifier = Modifier.height(20.dp))
@@ -53,94 +37,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-data class Message(val author: String, val body: String)
-
-@Composable
-fun MessageCard(msg: Message) {
-    Row(modifier = Modifier.padding(all = 8.dp)) {
-        Image(
-            painter = painterResource(R.drawable.test),
-            contentDescription = null,
-            modifier = Modifier
-                .size(40.dp)
-                .clip(CircleShape)
-                .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
-        )
-        Spacer(modifier = Modifier.width(8.dp))
-
-        // We keep track if the message is expanded or not in this
-        // variable
-        var isExpanded by remember { mutableStateOf(false) }
-        // surfaceColor will be updated gradually from one color to the other
-        val surfaceColor by animateColorAsState(
-            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
-        )
-
-        // We toggle the isExpanded variable when we click on this Column
-        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
-            Text(
-                text = msg.author,
-                color = MaterialTheme.colorScheme.secondary,
-                style = MaterialTheme.typography.titleSmall
-            )
-
-            Spacer(modifier = Modifier.height(4.dp))
-
-            Surface(
-                shape = MaterialTheme.shapes.medium,
-                shadowElevation = 1.dp,
-                // surfaceColor color will be changing gradually from primary to surface
-                color = surfaceColor,
-                // animateContentSize will change the Surface size gradually
-                modifier = Modifier.animateContentSize().padding(1.dp)
-            ) {
-                Text(
-                    text = msg.body,
-                    modifier = Modifier.padding(all = 4.dp),
-                    // If the message is expanded, we display all its content
-                    // otherwise we only display the first line
-                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
-                    style = MaterialTheme.typography.bodyMedium
-                )
-            }
-        }
-    }
-}
-
-@Preview(name = "Light Mode")
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    showBackground = true,
-    name = "Dark Mode"
-)
-@Composable
-fun PreviewMessageCard() {
-    ComposeTutorialTheme {
-        Surface {
-            MessageCard(
-                msg = Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!")
-            )
-        }
-    }
-}
-
-@Composable
-fun Conversation(messages: List<Message>) {
-    LazyColumn {
-        items(messages) { message ->
-            MessageCard(message)
-        }
-    }
-}
-
-@Preview
-@Composable
-fun PreviewConversation() {
-    ComposeTutorialTheme {
-        Conversation(SampleData.conversationSample)
-    }
-}
-
 @Serializable
 object Conversation
 @Serializable
@@ -148,6 +44,8 @@ object Profile
 
 @Composable
 fun NavHost(
+    //picture: File,
+    //name: File,
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController()
     ) {
@@ -156,6 +54,7 @@ fun NavHost(
         navController = navController,
         startDestination = Conversation
     ) {
+
         composable<Conversation> {
             ConversationScreen(
                 onNavigateToProfile = {
@@ -173,6 +72,8 @@ fun NavHost(
                         popUpTo(Conversation) { inclusive = true }
                     }
                 }
+                //picture = picture,
+                //name = name
             )
         }
     }
--- app/src/main/java/com/example/composetutorial/ProfileScreen.kt
+++ app/src/main/java/com/example/composetutorial/ProfileScreen.kt
@@ -1,8 +1,12 @@
 package com.example.composetutorial
 
-import androidx.compose.foundation.Image
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,48 +18,101 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.core.net.toUri
+import coil3.compose.AsyncImage
+import coil3.compose.AsyncImagePainter
+import coil3.compose.rememberAsyncImagePainter
+import coil3.request.ImageRequest
+import java.io.File
 
 @Composable
 fun ProfileScreen(
-    onNavigateToConversation: () -> Unit
+    onNavigateToConversation: () -> Unit,
+    //picture: File,
+    //name: File
 ) {
+
+    val context = LocalContext.current
+    val resolver = context.contentResolver
+    val picture = File(context.filesDir, "picture")
+    val name = File(context.filesDir, "name")
+    //var pictureUri = picture.toUri().toString() + "?timestamp=${System.currentTimeMillis()}"
+
+    var pictureUri by remember {
+        mutableStateOf(picture.toUri().toString() + "?timestamp=${System.currentTimeMillis()}")
+    }
+
+    var text by remember {
+        mutableStateOf(name.readBytes().decodeToString())
+    }
+
+    var pickedImage by remember {
+        mutableStateOf<Uri?>(null)
+    }
+
+    val pickMedia = rememberLauncherForActivityResult(ActivityResultContracts.PickVisualMedia()) {
+        pickedImage = it
+
+        if (it != null) {
+            resolver.openInputStream(it).use { stream ->
+                stream?.copyTo(picture.outputStream())
+                stream?.close()
+            }
+        }
+        pictureUri = picture.toUri().toString() + "?timestamp=${System.currentTimeMillis()}"
+    }
+
     Column {
         Spacer(
-            modifier = Modifier.fillMaxWidth().height(40.dp)
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(40.dp)
                 .background(color = MaterialTheme.colorScheme.secondary)
         )
 
         Row(modifier = Modifier.padding(8.dp)) {
-            Image(
-                painter = painterResource(R.drawable.test),
+            AsyncImage(
+                model = pictureUri,
                 contentDescription = null,
                 modifier = Modifier
                     .size(80.dp)
                     .clip(CircleShape)
                     .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
+                    .clickable { pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)) }
             )
 
             Column(modifier = Modifier.padding(8.dp)) {
                 Text(
-                    text = "Lexi",
+                    text = text,
                     color = MaterialTheme.colorScheme.secondary,
                     style = MaterialTheme.typography.titleLarge
                 )
-                Text(
-                    text = "User",
-                    color = MaterialTheme.colorScheme.tertiary,
-                    style = MaterialTheme.typography.titleMedium
-                )
             }
         }
+
+        TextField(
+            value = text,
+            onValueChange = { new ->
+                text = new
+                name.writeBytes(new.toByteArray())
+            },
+            modifier = Modifier.padding(horizontal = 8.dp)
+        )
     }
 
-    Button(onClick = onNavigateToConversation, modifier = Modifier.height(40.dp).padding(4.dp)) {
+    Button(onClick = onNavigateToConversation, modifier = Modifier
+        .height(40.dp)
+        .padding(4.dp)) {
         Text(text = "Back")
     }
 }
